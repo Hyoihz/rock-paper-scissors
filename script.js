6356e6d743395e3fac900ac1cc1d5bf992657af3
Raynald Escala <qwertyraynald@gmail.com>
2023-02-22 01:51:20 +0800

    Refactor counter functionality into separate function

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const playRound = (playerChoice, computerChoice) => {
     else if (playerChoice === "rock" && computerChoice === "scissors")
         winner = "player"
 
-    if (playerChoice === "paper" && computerChoice === "rock")
+    if (playerChoice === "paper" && computerChoice === "rock") 
         winner = "player"
     else if (playerChoice === "paper" && computerChoice === "scissors")
         winner = "computer"
@@ -31,6 +31,11 @@ const playRound = (playerChoice, computerChoice) => {
     return winner
 }
 
+const updateScores = (scores, roundWinner) => {
+    if (roundWinner === "player") scores.player++
+    else if (roundWinner === "computer") scores.computer++
+}
+
 const outputGameWinner = (playerScore, computerScore) => {
     if (playerScore > computerScore)
         console.log("%c Player Won!", "color: limegreen")
@@ -39,21 +44,17 @@ const outputGameWinner = (playerScore, computerScore) => {
     else console.log("%c Tie!", "color: gold")
 }
 
-
 const startRPSGame = (rounds = 1) => {
-    let playerWinCounter = 0
-    let computerWinCounter = 0
+    let scores = { player: 0, computer: 0 }
 
     for (let i = 0; i < rounds; i++) {
         const playerChoice = getPlayerChoice()
         const computerChoice = getComputerChoice()
 
         const roundWinner = playRound(playerChoice, computerChoice)
-        if (roundWinner === "player") playerWinCounter++
-        else if (roundWinner === "computer") computerWinCounter++
+        updateScores(scores, roundWinner)
     }
-
-    outputGameWinner(playerWinCounter, computerWinCounter)
+    outputGameWinner(scores.player, scores.computer)
 }
 
 startRPSGame(5)
